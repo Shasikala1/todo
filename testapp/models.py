c6e030c3d9522987361880a4b7a65428d815ef00
Shasi <nskala.soft@gmail.com>
2023-07-19 00:52:25 +0530

    add task functionality

--- testapp/models.py
+++ testapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Task(models.Model):
+    task=models.CharField(max_length=250)
+    is_completed=models.BooleanField(default=False)
+    created_at=models.DateField(auto_now_add=True) #only once created at starting
+    updated_at=models.DateField(auto_now=True) #everytime when u update 
+
 # Create your models here.
+    def __str__(self):
+        return self.task
--- testapp/views.py
+++ testapp/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from testapp.models import Task
 
 
 def home(request):
-    return render(request,'testapp/home.html')
+    tasks=Task.objects.filter(is_completed=False) # it filters all the tasks which are not completed
+    context={
+        'tasks':tasks
+    }
+
+    return render(request,'testapp/home.html',context)
+
+def addTask(request):
+    task=request.POST['task']
+    Task.objects.create(task=task)
+    return redirect(home)
 
 # Create your views here.
