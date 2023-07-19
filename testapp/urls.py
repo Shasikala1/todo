6635acc76c733a42290b40a81e8a94f3d2d4a5d2
Shasi <nskala.soft@gmail.com>
2023-07-19 14:55:49 +0530

    todo Final

--- testapp/urls.py
+++ testapp/urls.py
@@ -2,7 +2,17 @@ from django.urls import path
 from testapp import views
 
 urlpatterns = [
+    #add task
     path('addTask/', views.addTask ,name='addTask'),
+    #mark as done
+    path('mark_as_done/<int:pk>/', views.mark_as_done,name='mark_as_done'),
+    #mark as undone
+    path('mark_as_undone/<int:pk>/',views.mark_as_undone, name = 'mark_as_undone'),
+    #Edit feature
+    path('edit_task/<int:pk>/',views.edit_task,name = 'edit_task'),
+    #Delete task
+    path('delete_task/<int:pk>/',views.delete_task,name = 'delete_task'),
+
     
 
 ]
--- testapp/views.py
+++ testapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
 from testapp.models import Task
 
@@ -13,9 +13,43 @@ def home(request):
 
     return render(request,'testapp/home.html',context)
 
+#add task
 def addTask(request):
     task=request.POST['task']
     Task.objects.create(task=task)
     return redirect(home)
 
-# Create your views here.
+# mark as done
+def mark_as_done(request,pk):
+    task = get_object_or_404(Task,pk=pk) #fetch the data from databse if existe or it returns 404
+    task.is_completed=True
+    task.save()
+    return redirect('home')
+
+## mark as undone
+def mark_as_undone(request,pk):
+    task = get_object_or_404(Task,pk=pk) #fetch the data from databse if existe or it returns 404
+    task.is_completed=False
+    task.save()
+    return redirect('home')
+
+
+# edit task
+def edit_task(request,pk):
+    get_task=get_object_or_404(Task,pk=pk)
+    if request.method=='POST':
+        new_task=request.POST['task']
+        get_task.task=new_task
+        get_task.save()
+        return redirect('home')
+    else:
+        context={
+            'get_task':get_task,
+        }
+        return render(request,'testapp/edit_task.html',context)
+    #delete task
+
+def delete_task(request,pk):
+    task=get_object_or_404(Task,pk=pk)
+    task.delete()
+    return redirect('home')
